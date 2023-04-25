1730ded011c64944a063f925d84ac2f49569dd53
Liu <liut2@oregonstate..edu>
2023-04-25 13:10:25 -0700

    static setting

--- bookmanager/settings.py
+++ bookmanager/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#静态文件位置信息
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
